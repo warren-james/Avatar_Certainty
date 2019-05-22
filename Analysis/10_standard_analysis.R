fef19d27bdf6d71fbbfe9aa5ea475f3059a7390f
warren-james <w.james@abdn.ac.uk>
2019-05-22 10:55:59 +0100

    new script

--- Analysis/10_standard_analysis.R
+++ Analysis/10_standard_analysis.R
@@ -16,18 +16,20 @@ library(tidybayes)
 
 #### Load in Data ####
 load("scratch/data/df_Aberdeen_decisions")
+df_Aberdeen_decisions <- df_Aberdeen_decisions %>% 
+  mutate(dist_type = ifelse(delta < median(delta), "close", "far"))
 
 #### Mean and Variance in positioning ####
 # for each participant
 df_agg_p <- df_Aberdeen_decisions %>%
-  group_by(participant, condition_label, truck_perf, delta) %>% 
+  group_by(participant, condition_label, truck_perf, delta, dist_type) %>% 
   mutate(abs_pos = abs(placed_x)/delta) %>%
   summarise(mean_pos = mean(abs_pos),
             var_pos = var(abs_pos))
 
 # overall 
 df_agg_o <- df_agg_p %>%
-  group_by(condition_label, truck_perf, delta) %>% 
+  group_by(condition_label, truck_perf, delta, dist_type) %>% 
   summarise(n = n(), 
             position = mean(mean_pos),
             variance = mean(var_pos),
@@ -48,6 +50,22 @@ df_agg_o <- df_agg_p %>%
 # plot using overall data 
 plt_mean_pos <- df_agg_o %>%
   ggplot(aes(delta, position,
+             colour = condition_label,
+             fill = condition_label,
+             ymin = position - se_pos,
+             ymax = position + se_pos)) + 
+  geom_line() + 
+  geom_errorbar() + 
+  theme_minimal() + 
+  facet_wrap(~truck_perf)
+plt_mean_pos$labels$x <- "Delta (pixels)"
+plt_mean_pos$labels$y <- "Normalised Placement"
+plt_mean_pos$labsls$colour <- "Condition"
+plt_mean_pos$labsls$fill <- "Condition"
+plt_mean_pos
+
+plt_mean_pos_2 <- df_agg_o %>%
+  ggplot(aes(delta, position,
              colour = truck_perf,
              fill = truck_perf,
              ymin = position - se_pos,
@@ -56,11 +74,11 @@ plt_mean_pos <- df_agg_o %>%
   geom_errorbar() + 
   theme_minimal() + 
   facet_wrap(~condition_label)
-plt_mean_pos$labels$x <- "Delta (pixels)"
-plt_mean_pos$labels$y <- "Normalised Placement"
-plt_mean_pos$labsls$colour <- "Truck Performance"
-plt_mean_pos$labsls$fill <- "Truck Performance"
-plt_mean_pos
+plt_mean_pos_2$labels$x <- "Delta (pixels)"
+plt_mean_pos_2$labels$y <- "Normalised Placement"
+plt_mean_pos_2$labsls$colour <- "Truck Performance"
+plt_mean_pos_2$labsls$fill <- "Truck Performance"
+plt_mean_pos_2
 
 #### Plots: Variance ####
 plt_var_pos <- df_agg_o %>% 
@@ -75,9 +93,56 @@ plt_var_pos <- df_agg_o %>%
   facet_wrap(~condition_label)
 plt_var_pos$labels$x <- "Delta (pixels)"
 plt_var_pos$labels$y <- "Variance in  Placement"
-plt_var_pos$labsls$colour <- "Truck Performance"
-plt_var_pos$labsls$fill <- "Truck Performance"
+plt_var_pos$labels$colour <- "Truck Performance"
+plt_var_pos$labels$fill <- "Truck Performance"
 plt_var_pos
+
+plt_var_pos_2 <- df_agg_o %>% 
+  ggplot(aes(delta, variance,
+             colour = condition_label,
+             fill = condition_label,
+             ymin = variance - se_var,
+             ymax = variance + se_var)) + 
+  geom_line() + 
+  geom_errorbar() + 
+  theme_minimal() + 
+  facet_wrap(~truck_perf)
+plt_var_pos_2$labels$x <- "Delta (pixels)"
+plt_var_pos_2$labels$y <- "Variance in  Placement"
+plt_var_pos_2$labels$colour <- "Condition"
+plt_var_pos_2$labels$fill <- "Condition"
+plt_var_pos_2
+
+#### Plots: distribution plots ####
+# use all data and dist_type 
+plt_dist_all <- df_Aberdeen_decisions %>% 
+  mutate(abs_pos = abs(placed_x)/delta) %>%
+  filter(abs_pos <= 1) %>%
+  ggplot(aes(abs_pos,
+             colour = truck_perf,
+             fill = truck_perf)) + 
+  geom_density(alpha = 0.3) + 
+  facet_grid(condition_label~dist_type) + 
+  theme_minimal() + 
+  ggthemes::scale_color_ptol() + 
+  ggthemes::scale_fill_ptol()
+plt_dist_all
+
+
+# same as above using mean data 
+plt_dist_mean_p <- df_agg_p %>%
+  group_by(participant, condition_label, truck_perf, dist_type) %>%
+  summarise(mean_pos = mean(mean_pos)) %>%
+  ggplot(aes(mean_pos, 
+             colour = truck_perf,
+             fill = truck_perf)) + 
+  geom_density(alpha = 0.3) + 
+  facet_grid(condition_label~dist_type) + 
+  theme_minimal() + 
+  ggthemes::scale_color_ptol() + 
+  ggthemes::scale_fill_ptol()
+plt_dist_mean_p
+
 
 #### Analysis ####
 #### Analysis: ANOVAs ####
